b3951eca67a1cbfcbe62f018455b89ab46a5b544
KrishnaJ25 <krishnajodh25@gmail.com>
2026-06-21 00:37:03 +0530

    Add resume PDF and wire up resume button

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -75,7 +75,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer" download="Krishna_Jodh_Resume.pdf">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
